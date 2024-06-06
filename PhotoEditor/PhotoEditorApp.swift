d05d8cd0b2b12c5fd912515bab7f2b4fe78d8f48
Aleksander Kosarskii <kocagg@mail.ru>
2024-06-06 17:43:07 +0300

    Initial Commit

---
+++ PhotoEditor/PhotoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorApp.swift
+//  PhotoEditor
+//
+//  Created by Александр Косарский on 06.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
